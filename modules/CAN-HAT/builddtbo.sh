b6a1c887d370222393d8ee30fbeaebed41820060
Baozhu Zuo <zuobaozhu@gmail.com>
2018-11-13 08:30:19 +0000

    support 2-Channel CAN-BUS Shield

--- modules/CAN-HAT/builddtbo.sh
+++ modules/CAN-HAT/builddtbo.sh
@@ -1,6 +1,6 @@
 #dtoverlay -r mcp2517fd-can0
 
-dtc -@ -I dts -O dtb -o mcp2517fd-can0.dtbo mcp2517fd-can0-overlay.dts
+dtc -@ -I dts -O dtb -o 2xMCP2517FD.dtbo 2xMCP2517FD-overlay.dts
 
 # cp *.dtbo /boot/overlays
 # dtoverlay mcp2517fd-can0
