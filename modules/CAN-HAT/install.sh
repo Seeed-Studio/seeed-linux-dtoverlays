8387c46167863afc867793b29fc4f3c3d3c263db
turmary <turmary@126.com>
2019-11-01 11:10:11 +0000

    CAN-HAT fix the kernel version

--- modules/CAN-HAT/install.sh
+++ modules/CAN-HAT/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FORCE_KERNEL="1.20190925+1-1"
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root (use sudo)" 1>&2
    exit 1
@@ -13,17 +15,87 @@ fi
 
 ver="0.1"
 
-
 # we create a dir with this version to ensure that 'dkms remove' won't delete
 # the sources during kernel updates
 marker="0.0.0"
+
+_VER_RUN=
+function get_kernel_version() {
+  local ZIMAGE IMG_OFFSET
+
+  _VER_RUN=""
+  [ -z "$_VER_RUN" ] && {
+    ZIMAGE=/boot/kernel.img
+    IMG_OFFSET=$(LC_ALL=C grep -abo $'\x1f\x8b\x08\x00' $ZIMAGE | head -n 1 | cut -d ':' -f 1)
+    _VER_RUN=$(dd if=$ZIMAGE obs=64K ibs=4 skip=$(( IMG_OFFSET / 4)) | zcat | grep -a -m1 "Linux version" | strings | awk '{ print $3; }')
+  }
+  echo "$_VER_RUN"
+  return 0
+}
+
+function check_kernel_headers() {
+  VER_RUN=$(get_kernel_version)
+  VER_HDR=$(dpkg -L raspberrypi-kernel-headers | egrep -m1 "/lib/modules/[^-]+/build" | awk -F'/' '{ print $4; }')
+  [ "X$VER_RUN" == "X$VER_HDR" ] && {
+    return 0
+  }
+
+  # echo RUN=$VER_RUN HDR=$VER_HDR
+  echo " !!! Your kernel version is $VER_RUN"
+  echo "     Not found *** coressponding *** kernel headers with apt-get."
+  echo "     This may occur if you have ran 'rpi-update'."
+  echo " Choose  *** y *** will revert the kernel to version $VER_HDR then continue."
+  echo " Choose  *** N *** will exit without this driver support, by default."
+  read -p "Would you like to proceed? (y/N)" -n 1 -r -s
+  echo
+  if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    exit 1;
+  fi
+
+  apt-get -y --reinstall install raspberrypi-kernel
+}
+
+function download_install_debpkg() {
+  local prefix name r
+  prefix=$1
+  name=$2
+
+  for (( i = 0; i < 3; i++ )); do
+    wget $prefix$name -O /tmp/$name && break
+  done
+  dpkg -i /tmp/$name; r=$?
+  rm -f /tmp/$name
+  return $r
+}
+
+function install_kernel() {
+  local _url _prefix
+
+   # Instead of retriving the lastest kernel & headers
+  [ "X$FORCE_KERNEL" == "X" ] && {
+    apt-get -y --force-yes install raspberrypi-kernel-headers raspberrypi-kernel
+  } || {
+    # We would like to a fixed version
+    KERN_NAME=raspberrypi-kernel_${FORCE_KERNEL}_armhf.deb
+    HDR_NAME=raspberrypi-kernel-headers_${FORCE_KERNEL}_armhf.deb
+    _url=$(apt-get download --print-uris raspberrypi-kernel | sed -nre "s/'([^']+)'.*$/\1/g;p")
+    _prefix=$(echo $_url | sed -nre 's/^(.*)raspberrypi-kernel_.*$/\1/g;p')
+
+    download_install_debpkg "$_prefix" "$KERN_NAME"
+    download_install_debpkg "$_prefix" "$HDR_NAME"
+  }
+}
 
 # update and install required packages
 which apt &>/dev/null
 if [[ $? -eq 0 ]]; then
   apt update -y
-  apt-get -y install raspberrypi-kernel-headers raspberrypi-kernel 
+  echo "____________________________________"
   apt-get -y install dkms git can-utils
+  echo "____________________________________"
+  install_kernel
+  # rpi-update checker
+  check_kernel_headers
 fi
 
 # Arch Linux
@@ -34,10 +106,13 @@ fi
 
 # locate currently installed kernels (may be different to running kernel if
 # it's just been updated)
-kernels=$(ls /lib/modules | sed "s/^/-k /")
-uname_r=$(uname -r)
+base_ver=$(get_kernel_version)
+base_ver=${base_ver%%[-+]*}
+kernels="${base_ver}+ ${base_ver}-v7+ ${base_ver}-v7l+"
 
 function install_module {
+  local _i
+
   src=$1
   mod=$2
 
@@ -49,10 +124,16 @@ function install_module {
     dkms remove --force -m $mod -v $ver --all
     rm -rf /usr/src/$mod-$ver
   fi
+
   mkdir -p /usr/src/$mod-$ver
   cp -a $src/* /usr/src/$mod-$ver/
+
   dkms add -m $mod -v $ver
-  dkms build $kernels -m $mod -v $ver && dkms install --force $kernels -m $mod -v $ver
+  for _i in $kernels; do
+    dkms build -k $_i -m $mod -v $ver && {
+      dkms install --force -k $_i -m $mod -v $ver
+    }
+  done
 
   mkdir -p /var/lib/dkms/$mod/$ver/$marker
 }
