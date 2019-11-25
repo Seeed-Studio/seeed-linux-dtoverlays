e470c8c26dd3aab1306fd6cf6eee9f29138e08f4
Jens Hertz <jens.hertz@magnitudespace.com>
2019-11-25 22:19:43 +0100

    Add 2xMCP2518FD-overlay.dts

--- modules/CAN-HAT/builddtbo.sh
+++ modules/CAN-HAT/builddtbo.sh
@@ -1,6 +1,7 @@
 #dtoverlay -r mcp2517fd-can0
 
 dtc -@ -I dts -O dtb -o 2xMCP2517FD.dtbo 2xMCP2517FD-overlay.dts
+dtc -@ -I dts -O dtb -o 2xMCP2518FD.dtbo 2xMCP2518FD-overlay.dts
 
 # cp *.dtbo /boot/overlays
 # dtoverlay mcp2517fd-can0
