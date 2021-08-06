2d5184900fe88b2379214463a06621182e884ede
953308023@qq.com <953308023@qq.com>
2021-08-06 11:12:25 +0800

    Revert "Revert "auto reset the MCU when the raspberrypi CPU is reseted by the key""
    This reverts commit 5da752afb4a710301c03c77e78ea0395b3d0fb45.

--- extras/reTerminal/STM32G030F6_R2/Core/Src/stm32g0xx_it.c
+++ extras/reTerminal/STM32G030F6_R2/Core/Src/stm32g0xx_it.c
@@ -58,6 +58,7 @@
 /* External variables --------------------------------------------------------*/
 extern DMA_HandleTypeDef hdma_adc1;
 extern I2C_HandleTypeDef hi2c1;
+extern TIM_HandleTypeDef htim14;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -153,6 +154,20 @@ void DMA1_Channel1_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
 
   /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM14 global interrupt.
+  */
+void TIM14_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM14_IRQn 0 */
+
+  /* USER CODE END TIM14_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim14);
+  /* USER CODE BEGIN TIM14_IRQn 1 */
+
+  /* USER CODE END TIM14_IRQn 1 */
 }
 
 /**
