b4412989ae4ad8acf530d3999dd2256c8888f809
bigbearishappy <953308023@qq.com>
2024-03-14 06:20:49 +0000

    ili9881d compatible with kernel version 6.6

--- modules/ili9881d/ili9881d.c
+++ modules/ili9881d/ili9881d.c
@@ -498,7 +498,9 @@ static int ili9881d_dsi_probe(struct mipi_dsi_device *dsi)
 	ctx->dsi = dsi;
 	ctx->desc = of_device_get_match_data(&dsi->dev);
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 6, 20)
+	ctx->panel.prepare_prev_first = true;
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 	ctx->panel.prepare_upstream_first = true;
 #endif
 	drm_panel_init(&ctx->panel, &dsi->dev, &ili9881d_funcs,
